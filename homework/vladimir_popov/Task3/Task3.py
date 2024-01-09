5ba92deceafb1818702783096ce591d4cf2e319d
Wladdima <wladdima.wladimir@gmail.com>
2024-01-09 19:46:45 +0100

    lint errors fixed

--- homework/vladimir_popov/Task3/Task3.py
+++ homework/vladimir_popov/Task3/Task3.py
@@ -1,6 +1,8 @@
 
 
-my_dict = {"tuple": (1,2,2,3,5), "list": [1,2,3,4,5], "dict": {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}, "set": {1,2,3,4,5}}
+my_dict = {"tuple": (1, 2, 2, 3, 5), "list": [1, 2, 3, 4, 5], "dict": 
+           {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}, "set": {1, 2, 3, 4, 5}
+           }
 
 print(my_dict["tuple"][-1])
 my_dict["list"].append(6)
