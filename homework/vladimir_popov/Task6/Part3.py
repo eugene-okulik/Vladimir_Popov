415925775338538f832c0557366f56d56f2e65b2
Wladdima <wladdima.wladimir@gmail.com>
2024-01-16 22:01:34 +0100

    lint error fixed

--- homework/vladimir_popov/Task6/Part3.py
+++ homework/vladimir_popov/Task6/Part3.py
@@ -1,7 +1,8 @@
 
 def plus_ten(str):
-    text, digit =  str.split(":")
+    text, digit = str.split(":")
     print(f"{text}: {int(digit) + 10}")
+
 
 plus_ten("результат операции: 42")
 plus_ten("результат операции: 54")
