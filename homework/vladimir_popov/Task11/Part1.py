51b393c429baf2b1c71cc2706f32b323dacb29f8
Wladdima <wladdima.wladimir@gmail.com>
2024-01-25 21:31:33 +0100

    Lint errors fixed

--- homework/vladimir_popov/Task11/Part1.py
+++ homework/vladimir_popov/Task11/Part1.py
@@ -10,13 +10,18 @@ class Book:
         self.page_count = page_count
         self.isbn = isbn
         self.reserved = reserved
-    
 
     def book_info(self):
         if self.reserved:
-            print(f"Название: {self.title}, Автор: {self.author}, страниц: {self.page_count}, материал: {self.material}, зарезервирована")
+            print(
+                f"Название: {self.title}, Автор: {self.author},
+                страниц: {self.page_count}, материал: {self.material}, зарезервирована"
+            )
         else:
-            print(f"Название: {self.title}, Автор: {self.author}, страниц: {self.page_count}, материал: {self.material}")
+            print(
+                f"Название: {self.title}, Автор: {self.author},
+                страниц: {self.page_count}, материал: {self.material}"
+            )
 
 
 book_1 = Book("Идиот", "Достоевский", 500, 12345, False)
@@ -40,17 +45,28 @@ class School_Book(Book):
         self.school_class = school_class
         self.tasks_availability = tasks_availability
 
-
     def book_info(self):
         if self.reserved:
-            print(f"Название: {self.title}, Автор: {self.author}, страниц: {self.page_count}, предмет: {self.subject}, класс: {self.school_class}, зарезервирована")
+            print(
+                f"Название: {self.title}, Автор: {self.author},
+                страниц: {self.page_count}, предмет: {self.subject}, класс: {self.school_class}, зарезервирована"
+            )
         else:
-            print(f"Название: {self.title}, Автор: {self.author}, страниц: {self.page_count}, предмет: {self.subject}, класс: {self.school_class}")
+            print(
+                f"Название: {self.title}, Автор: {self.author},
+                страниц: {self.page_count}, предмет: {self.subject}, класс: {self.school_class}"
+            )
 
 
-school_book1 = School_Book(title="Алгебра", author="Иванов", page_count=200, isbn=12345, tasks_availability=True, subject="Математика", school_class=9, reserved=False)
-school_book2 = School_Book(title="История Казахстана", author="Алимов", page_count=250, isbn=23456, tasks_availability=False, subject="История", school_class=9, reserved=True)
-school_book3 = School_Book(title="English", author="Петров", page_count=150, isbn=34567, tasks_availability=True, subject="Английский язык", school_class=6, reserved=False)
+school_book1 = School_Book(title="Алгебра", author="Иванов",
+                           page_count=200, isbn=12345, tasks_availability=True, subject="Математика", school_class=9, reserved=False
+                           )
+school_book2 = School_Book(title="История Казахстана", author="Алимов", 
+                           page_count=250, isbn=23456, tasks_availability=False, subject="История", school_class=9, reserved=True
+                           )
+school_book3 = School_Book(title="English", author="Петров", 
+                           page_count=150, isbn=34567, tasks_availability=True, subject="Английский язык", school_class=6, reserved=False
+                           )
 
 school_book1.book_info()
 school_book2.book_info()
