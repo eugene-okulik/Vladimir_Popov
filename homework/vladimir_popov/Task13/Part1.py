534121d6485bca61bf58041bc02c0fb6cc94fa58
Wladdima <wladdima.wladimir@gmail.com>
2024-02-01 20:40:11 +0100

    Lint error fixed

--- homework/vladimir_popov/Task13/Part1.py
+++ homework/vladimir_popov/Task13/Part1.py
@@ -13,6 +13,7 @@ def read_file():
             index_date_time, *task = str.split(" - ")
             yield index_date_time.split(". ")
 
+
 week_days = {1: "понедельник", 2: "вторник", 3: "среда", 4: "четверг", 5: "пятница", 6: "суббота", 7: "воскресенье"}
 
 now = datetime.datetime.now()
