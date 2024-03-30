28478d832cb08ec68a61b53f17a0bad8464f310b
Wladdima <wladdima.wladimir@gmail.com>
2024-03-30 22:30:53 +0100

    lint errors fixed

--- homework/vladimir_popov/Task22/locustfile.py
+++ homework/vladimir_popov/Task22/locustfile.py
@@ -3,7 +3,6 @@ from locust import task, HttpUser
 
 class NewUser(HttpUser):
 
-
     url = "https://api.restful-api.dev"
     body = {
             "name": "Apple MacBook Pro 16",
@@ -13,23 +12,19 @@ class NewUser(HttpUser):
                 "CPU model": "Intel Core i9",
                 "Hard disk size": "1 TB"
             }
-        }
-
+    }
 
     def on_start(self):
         response = self.client.post("/objects", json=self.body)
         self.object_id = response.json()['id']
 
-
     @task(1)
     def get_all_posts(self):
         self.client.get("/objects")
 
-
     @task(3)
     def get_one_object(self):
         self.client.get(f"/objects/{self.object_id}")
-
 
     @task(2)
     def add_new_post(self):
