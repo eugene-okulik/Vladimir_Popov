1a414e76f60c304e33aa72514acc3ad686345fcf
Wladdima <wladdima.wladimir@gmail.com>
2024-03-30 22:34:04 +0100

    lint errors fixed

--- homework/vladimir_popov/Task22/locustfile.py
+++ homework/vladimir_popov/Task22/locustfile.py
@@ -5,13 +5,13 @@ class NewUser(HttpUser):
 
     url = "https://api.restful-api.dev"
     body = {
-            "name": "Apple MacBook Pro 16",
-            "data": {
-                "year": 2019,
-                "price": 1849.99,
-                "CPU model": "Intel Core i9",
-                "Hard disk size": "1 TB"
-            }
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
     }
 
     def on_start(self):
